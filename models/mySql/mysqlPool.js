b7975f73bff36c3f918a992836721cd096170f6f
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-07-22 06:12:17 +0300

    init connection pool

--- models/mySql/mysqlPool.js
+++ models/mySql/mysqlPool.js
@@ -1,10 +1,13 @@
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config();
 
-let pool = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.SQL_HOST,
   user: process.env.SQL_USER,
   password: process.env.SQL_PASSWORD,
-  database: process.env.SQL_DB,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+
+//get all users
+router.get("/", async (req, res, next) => {
+  try {
+    res.json({ msg: "nodeProject" });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
