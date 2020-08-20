8638ed45440564b88081c7834a4b6601d32ad84e
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-08-21 01:41:33 +0300

    Create mysqlPool.js

---
+++ models/mySql/mysqlPool.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.SQL_HOST,
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool.promise();
