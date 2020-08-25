0bdbc16602fa9be0bc7bf16fdd25702f54c62c75
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-08-26 02:43:40 +0300

    commit

--- routes/shoppingCart.js
+++ routes/shoppingCart.js
@@ -26,18 +26,11 @@ router.get("/getCart", async (req, res, next) => {
 router.get("/add/:id", async (req, res, next) => {
   try {
     let productId = req.params.id;
-    console.log("add to cart product id = " + productId);
     let cart = await new Cart(req.session.cart ? req.session.cart : {});
     let [product] = await Items.getItemByID(productId);
-    console.log("cart product", product[0]);
-    // let [products] = await getAllItems();
-    // let product = products.filter((item) => {
-    //   return item.item_id == productId;
-    // });
     cart.add(product[0], productId);
     req.session.cart = cart;
     res.redirect("/");
-    console.log("after redirect");
   } catch (err) {
     res.status(400).json(err);
   }
@@ -54,11 +47,5 @@ router.get("/remove/:id", async (req, res, next) => {
     res.status(400).json(err);
   }
 });
-
-// router.get("/checkout", (req, res) => {
-//   res.render("shop/checkout", {
-//     cart: req.session.cart,
-//   });
-// });
 
 module.exports = router;
