9f29d3f62eace1c45200b61d8cb9c8388aea137f
tbookra <tbookra@gmail.com>
2020-08-27 23:34:49 +0300

    repairing the pushed out code
    search bar, and some code fixing restored.

--- routes/electric.js
+++ routes/electric.js
@@ -1,43 +1,38 @@
 const express = require("express");
 const authMiddleware = require("../middleware/auth");
-const Items = require("../models/mySql/Items");
+const Items = require('../models/mySql/Items');
 const router = express.Router();
 
 // router.use(authMiddleware);
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  try {
-    // let userList = await clients.selectUsers();
-    // module.exports.userList = userList[0];
-    let productsInfo = await Items.getItemsByCategory("electric");
-    //  let productsInfo = await clients.getData('"electric"');
-    let arr = productsInfo[0];
-    module.exports.electricInfo = productsInfo[0];
-    if (req.query.q) {
+  try{
+  let productsInfo = await Items.getItemsByCategory('electric');
+  let arr = productsInfo[0];
+  module.exports.electricInfo = productsInfo[0];
+ 
+  if (req.query.search) {
       arr = productsInfo[0].filter((item) => {
-        return item.product_name.search(req.query.q) >= 0;
-      });
-    }
-    res.render("electric", {
-      title: "Express",
-      ...req.nav,
-      electricarr: arr,
+      return item.item_name.search(req.query.search) >= 0;
     });
-  } catch (e) {
-    console.log(e);
   }
+    res.render("electric", {
+    title: "Express",
+    ...req.nav,
+    electricarr: arr,
+  });
+} catch (e) {
+  console.log(e);
+}
+
 });
 
-router.get("/auth/:item_id", authMiddleware, (req, res) => {
-  let ttt = module.exports.electricInfo;
-  let place = ttt.find((elm) => elm.item_id == req.params.item_id);
-
-  res.render("place_ditales", {
-    ...req.nav,
-    title: place.item_name,
-    place: place,
-  });
+router.get("/:item_id", authMiddleware ,(req, res) => {
+  let electricInfoArr = module.exports.electricInfo
+  let place = electricInfoArr.find((elm) => elm.item_id == req.params.item_id);
+  
+  res.render("place_ditales", { ...req.nav, title: place.item_name, place: place });
 });
 
 module.exports = router;
--- routes/mountain.js
+++ routes/mountain.js
@@ -1,39 +1,36 @@
 const express = require("express");
-const router = express.Router();
 const authMiddleware = require("../middleware/auth");
-const Items = require("../models/mySql/Items");
+const Items = require('../models/mySql/Items');
+const router = express.Router();
+
+// router.use(authMiddleware);
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  try {
-    let productsInfo = await Items.getItemsByCategory("mountain");
-    // console.log("productsInfo", productsInfo[0]);
-    // let productsInfo = await clients.getData('"mountain"');
-    let arr = productsInfo[0];
-    module.exports.mountainInfo = productsInfo[0];
-    if (req.query.q) {
-      arr = productsInfo[0].filter((item) => {
-        return item.product_name.search(req.query.q) >= 0;
-      });
-    }
-    res.render("mountain", {
-      title: "Express",
-      ...req.nav,
-      mountarr: arr,
+  try{
+  let productsInfo = await Items.getItemsByCategory('mountain');
+  let arr = productsInfo[0];
+  module.exports.mountainInfo = productsInfo[0];
+  if (req.query.search) {
+    arr = productsInfo[0].filter((item) => {
+      return item.item_name.search(req.query.search) >= 0;
     });
-  } catch (e) {
-    console.log(e);
   }
+  res.render("mountain", {
+    title: "Express",
+    ...req.nav,
+    mountarr: arr,
+  });
+} catch (e) {
+  console.log(e);
+}
+
 });
 
-router.get("/auth/:item_id", authMiddleware, (req, res) => {
-  let ttt = module.exports.mountainInfo;
-  let place = ttt.find((elm) => elm.item_id == req.params.item_id);
-  res.render("place_ditales", {
-    ...req.nav,
-    title: place.item_name,
-    place: place,
-  });
+router.get("/:item_id", authMiddleware ,(req, res) => {
+  let mountainInfoArr = module.exports.mountainInfo
+  let place = mountainInfoArr.find((elm) => elm.item_id == req.params.item_id);
+  res.render("place_ditales", { ...req.nav, title: place.item_name, place: place });
 });
 
 module.exports = router;
