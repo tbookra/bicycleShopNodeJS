f062d6918df84ee933887f2af077339f1ede3405
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-08-27 15:55:04 +0300

    Update electric.js

--- routes/electric.js
+++ routes/electric.js
@@ -1,42 +1,43 @@
 const express = require("express");
 const authMiddleware = require("../middleware/auth");
-const clients = require('../models/mySql/clients');
-const Items = require('../models/mySql/Items');
-const app = require("../app");
+const Items = require("../models/mySql/Items");
 const router = express.Router();
 
 // router.use(authMiddleware);
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  try{
-    let userList = await clients.selectUsers();
-    module.exports.userList = userList[0];
-    let productsInfo = await Items.getItemsByCategory('electric');
-//  let productsInfo = await clients.getData('"electric"');
-  let arr = productsInfo[0];
-  module.exports.electricInfo = productsInfo[0];
-  if (req.query.q) {
-    arr = productsInfo[0].filter((item) => {
-      return item.product_name.search(req.query.q) >= 0;
-    });
-  }
+  try {
+    // let userList = await clients.selectUsers();
+    // module.exports.userList = userList[0];
+    let productsInfo = await Items.getItemsByCategory("electric");
+    //  let productsInfo = await clients.getData('"electric"');
+    let arr = productsInfo[0];
+    module.exports.electricInfo = productsInfo[0];
+    if (req.query.q) {
+      arr = productsInfo[0].filter((item) => {
+        return item.product_name.search(req.query.q) >= 0;
+      });
+    }
     res.render("electric", {
-    title: "Express",
-    ...req.nav,
-    electricarr: arr,
-  });
-} catch (e) {
-  console.log(e);
-}
-
+      title: "Express",
+      ...req.nav,
+      electricarr: arr,
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
-router.get("/auth/:item_id", authMiddleware ,(req, res) => {
-  let ttt = module.exports.electricInfo
+router.get("/auth/:item_id", authMiddleware, (req, res) => {
+  let ttt = module.exports.electricInfo;
   let place = ttt.find((elm) => elm.item_id == req.params.item_id);
-  
-  res.render("place_ditales", { ...req.nav, title: place.item_name, place: place });
+
+  res.render("place_ditales", {
+    ...req.nav,
+    title: place.item_name,
+    place: place,
+  });
 });
 
 module.exports = router;
